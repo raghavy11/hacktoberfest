11a438f5696cb49c7646b8fe547a13c24ed07257
raghavy11 <raghavydav1111@gmail.com>
2024-10-20 15:36:35 +0530

    factorial

---
+++ java/Factorial.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Factorial {
+
+    // Method to calculate factorial recursively
+    public static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+
+        int result = factorial(num);
+        System.out.println("Factorial of " + num + " is: " + result);
+    }
+}
